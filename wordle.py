2aa82a30ad722a9f8e75a7f388767f2fda88aae3
milesreng <miles.eng@duke.edu>
2022-02-01 20:27:26 -0500

    added functionality for multiple games and corrected update_pointers method

--- wordle.py
+++ wordle.py
@@ -6,9 +6,12 @@ def get_wordle(words):
 
 def update_pointers(guess, correct, pointers):
     for i in range(len(guess)):
-        if guess[i] == correct[i]:
-            pointers[i] = guess[i]
-    return
+        in_word = letter_in_word(guess[i], correct)
+        if in_word == 0:
+            pointers[i] = "^"
+        elif in_word == 1:
+            pointers[i] = "|"
+    return pointers
 
 def letter_in_word(letter, word):
     for ch in word:
@@ -24,6 +27,9 @@ def run_game(words):
     correct = False
     guesses = 6
     guessed_words = []
+    letters_left = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                    'W', 'X', 'Y', 'Z']
 
     while guesses > 0 and not correct:
         valid_guess = False
@@ -41,10 +47,37 @@ def run_game(words):
             else:
                 guessed_words.append(guess)
                 valid_guess = True
-        
+        pointers = update_pointers(guess, wordle, pointers)
+        template = [ch for ch in guess]
+        print(" ".join(template))
+        print(" ".join(pointers))
+
+        if guess == wordle:
+            correct = True
+
         guesses -= 1
+    if correct:
+        print("nice! you guessed it in " + str(guesses) + "/6 guesses!")
+    else:
+        print("sorry, word was " + wordle)
+
+    return correct
+
 
 if __name__ == "__main__":
     with open("fiveletterwords.txt") as file:
         valid_words = [line.rstrip().upper() for line in file]
-    run_game(valid_words)
+
+    wins = 0
+    games = 0
+    play_game = True
+    while play_game:
+        win = run_game(valid_words)
+        again = input("do you want to play again? y/n: ")
+        if again != "y" or again != "Y":
+            play_game = False
+        if win:
+            wins += 1
+        games += 1
+
+    print("you played " + str(games) + " games and won " + str(wins) + " times")
